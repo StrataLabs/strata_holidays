8cbb8f8613ea96f048d4582d2b24204ad938ad09
vishnu <vishnu@vishnu-HP.(none)>
2013-06-17 14:03:46 +0530

    Initial Commit - Basic Setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+StrataHolidays::Application.config.secret_key_base = '9c89d9d7c4a6fd59fa2f36ddc04512a6550161e37fa7477f142971a84b9aa3ea17818079a166d6e2e5730e03dce78e9da38db1addf3a7d12286e0a31e4957c7c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+StrataHolidays::Application.config.session_store :cookie_store, key: '_strata_holidays_session'
