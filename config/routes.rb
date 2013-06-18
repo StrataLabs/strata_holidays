cab453b43f5b31dec6f121158d3619723773cc21
advishnuprasad <advishnuprsad@gmail.com>
2013-06-18 12:09:17 +0530

    Updating Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 StrataHolidays::Application.routes.draw do
+  devise_for :users
   resources :properties
 
   resources :poa_types
@@ -7,7 +8,6 @@ StrataHolidays::Application.routes.draw do
 
   resources :destinations
 
-  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
