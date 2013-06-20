dc0d22fedf1f0bd6d408ecdf854e071bf8071548
advishnuprasad <advishnuprsad@gmail.com>
2013-06-20 16:03:23 +0530

    Adding Validation for Vacation Types

--- app/models/vacation_type.rb
+++ app/models/vacation_type.rb
@@ -1,3 +1,4 @@
 class VacationType < ActiveRecord::Base
   validates_presence_of :vac_type
+  validates :vac_type, presence: true, length: {minimum:3, maximum: 80}
 end
--- spec/models/vacation_type_spec.rb
+++ spec/models/vacation_type_spec.rb
@@ -6,5 +6,13 @@ describe VacationType do
       subject { FactoryGirl.create(:vacation_type) }
       it { should validate_presence_of :vac_type  }
     end
+    it "should validate length" do
+      @dest = VacationType.new(:vac_type => "fo")
+      @dest.should_not be_valid
+      @dest = VacationType.new(:vac_type => "foodoo")
+      @dest.should be_valid
+      @dest = VacationType.new(:vac_type => "foodoo"*100)
+      @dest.should_not be_valid
+    end
   end
 end
