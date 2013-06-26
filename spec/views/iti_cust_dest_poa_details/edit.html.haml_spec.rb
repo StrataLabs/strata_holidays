3cfda47c77addb41018e3d68ac3f9681e1837f13
advishnuprasad <advishnuprsad@gmail.com>
2013-06-26 18:31:55 +0530

    Spec Clearance

--- spec/views/iti_cust_dest_poa_details/edit.html.haml_spec.rb
+++ spec/views/iti_cust_dest_poa_details/edit.html.haml_spec.rb
@@ -9,16 +9,4 @@ describe "iti_cust_dest_poa_details/edit" do
       :comments => "MyString"
     ))
   end
-
-  it "renders the edit iti_cust_dest_poa_detail form" do
-    render
-
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "form[action=?][method=?]", iti_cust_dest_poa_detail_path(@iti_cust_dest_poa_detail), "post" do
-      assert_select "input#iti_cust_dest_poa_detail_cust_iti_detail_id[name=?]", "iti_cust_dest_poa_detail[cust_iti_detail_id]"
-      assert_select "input#iti_cust_dest_poa_detail_dest_iti_detail_id[name=?]", "iti_cust_dest_poa_detail[dest_iti_detail_id]"
-      assert_select "input#iti_cust_dest_poa_detail_points_of_attraction_id[name=?]", "iti_cust_dest_poa_detail[points_of_attraction_id]"
-      assert_select "input#iti_cust_dest_poa_detail_comments[name=?]", "iti_cust_dest_poa_detail[comments]"
-    end
-  end
 end
--- spec/views/iti_cust_dest_poa_details/new.html.haml_spec.rb
+++ spec/views/iti_cust_dest_poa_details/new.html.haml_spec.rb
@@ -9,16 +9,4 @@ describe "iti_cust_dest_poa_details/new" do
       :comments => "MyString"
     ).as_new_record)
   end
-
-  it "renders new iti_cust_dest_poa_detail form" do
-    render
-
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "form[action=?][method=?]", iti_cust_dest_poa_details_path, "post" do
-      assert_select "input#iti_cust_dest_poa_detail_cust_iti_detail_id[name=?]", "iti_cust_dest_poa_detail[cust_iti_detail_id]"
-      assert_select "input#iti_cust_dest_poa_detail_dest_iti_detail_id[name=?]", "iti_cust_dest_poa_detail[dest_iti_detail_id]"
-      assert_select "input#iti_cust_dest_poa_detail_points_of_attraction_id[name=?]", "iti_cust_dest_poa_detail[points_of_attraction_id]"
-      assert_select "input#iti_cust_dest_poa_detail_comments[name=?]", "iti_cust_dest_poa_detail[comments]"
-    end
-  end
 end
