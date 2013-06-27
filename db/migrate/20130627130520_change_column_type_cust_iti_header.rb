62919578fab1d6b088eda9764bd7dbb367d4b272
advishnuprasad <advishnuprsad@gmail.com>
2013-06-27 19:05:16 +0530

    Changing migration script for PG

--- db/migrate/20130627130520_change_column_type_cust_iti_header.rb
+++ db/migrate/20130627130520_change_column_type_cust_iti_header.rb
@@ -1,5 +1,9 @@
 class ChangeColumnTypeCustItiHeader < ActiveRecord::Migration
   def change
-    change_column :cust_iti_headers, :no_of_children, :integer
+    # change_column :cust_iti_headers, :no_of_children, :integer
+    execute <<-SQL
+      ALTER TABLE cust_iti_headers
+      ALTER COLUMN no_of_children TYPE integer USING no_of_children::integer
+    SQL
   end
 end
