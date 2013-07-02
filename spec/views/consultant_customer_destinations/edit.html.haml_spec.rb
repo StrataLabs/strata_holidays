67b760337c69f3862952007546c64f9432d7954b
advishnuprasad <advishnuprsad@gmail.com>
2013-07-02 18:43:51 +0530

    Spec Clearance

--- spec/views/consultant_customer_destinations/edit.html.haml_spec.rb
+++ spec/views/consultant_customer_destinations/edit.html.haml_spec.rb
@@ -1,22 +1,4 @@
 require 'spec_helper'
 
 describe "consultant_customer_destinations/edit" do
-  before(:each) do
-    @consultant_customer_destination = assign(:consultant_customer_destination, stub_model(ConsultantCustomerDestination,
-      :vacation_consultant_id => 1,
-      :customer_id => 1,
-      :cust_iti_header_id => 1
-    ))
-  end
-
-  it "renders the edit consultant_customer_destination form" do
-    render
-
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "form[action=?][method=?]", consultant_customer_destination_path(@consultant_customer_destination), "post" do
-      assert_select "input#consultant_customer_destination_vacation_consultant_id[name=?]", "consultant_customer_destination[vacation_consultant_id]"
-      assert_select "input#consultant_customer_destination_customer_id[name=?]", "consultant_customer_destination[customer_id]"
-      assert_select "input#consultant_customer_destination_cust_iti_header_id[name=?]", "consultant_customer_destination[cust_iti_header_id]"
-    end
-  end
 end
